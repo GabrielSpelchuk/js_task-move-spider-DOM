3c5cb7affbd761190d883b82963fdf27cd8f54db
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-18 15:45:42 +0200

    fixed bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,8 +18,8 @@ document.addEventListener('click', (e) => {
 
   const minX = 0;
   const minY = 0;
-  const maxX = wall.offsetWidth - spiderWidth - wall.clientLeft * 2;
-  const maxY = wall.offsetHeight - spiderHeight - wall.clientTop * 2;
+  const maxX = wall.offsetWidth - spiderWidth;
+  const maxY = wall.offsetHeight - spiderHeight;
 
   x = Math.max(minX, Math.min(x, maxX));
   y = Math.max(minY, Math.min(y, maxY));
